6f872d5c920cefe6ee49d9a7eb930cb8a027b507
dstrodtman-db <50596795+dstrodtman-db@users.noreply.github.com>
2021-06-16 13:44:05 -0400

    Update my_funcs.py

--- my_lib/my_funcs.py
+++ my_lib/my_funcs.py
@@ -1,4 +1,3 @@
-# Databricks notebook source
 def convertFtoC(unitCol, tempCol):
     from pyspark.sql.functions import when, col
     return when(col(unitCol) == "F", (col(tempCol) - 32) * (5/9)).otherwise(col(tempCol)).alias("temp_celcius")
@@ -6,7 +5,3 @@ def convertFtoC(unitCol, tempCol):
 def roundedTemp(unitCol, tempCol):
     from pyspark.sql.functions import round, concat_ws
     return concat_ws(" ", round(tempCol, 2).cast("string"), unitCol).alias("rounded_temp")
-
-# COMMAND ----------
-
-
