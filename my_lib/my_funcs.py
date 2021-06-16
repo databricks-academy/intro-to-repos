704b617387d602fc044d572b083bc49980af70e3
dstrodtman-db <dstrodtman-db>
2021-06-16 17:43:24 +0000

    adding library

---
+++ my_lib/my_funcs.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+def convertFtoC(unitCol, tempCol):
+    from pyspark.sql.functions import when, col
+    return when(col(unitCol) == "F", (col(tempCol) - 32) * (5/9)).otherwise(col(tempCol)).alias("temp_celcius")
+
+def roundedTemp(unitCol, tempCol):
+    from pyspark.sql.functions import round, concat_ws
+    return concat_ws(" ", round(tempCol, 2).cast("string"), unitCol).alias("rounded_temp")
+
+# COMMAND ----------
+
+
---
+++ temperatures.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %run ./my_funcs
+
+# COMMAND ----------
+
+import numpy as np
+import pandas as pd
+
+Fdf = pd.DataFrame(np.random.normal(55, 25, 10000), columns=["temp"])
+Fdf["unit"] = "F"
+
+Cdf = pd.DataFrame(np.random.normal(10, 10, 10000), columns=["temp"])
+Cdf["unit"] = "C"
+
+df = spark.createDataFrame(pd.concat([Fdf, Cdf]))
+
+display(df)
+
+# COMMAND ----------
+
+display(df.select(roundedTemp("unit", "temp")))
+
+# COMMAND ----------
+
+display(df.select(convertFtoC("unit", "temp")))
