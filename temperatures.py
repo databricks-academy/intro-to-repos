770094421fa0cf8b6c0e4c8ea9658c1b2dec39a7
abhi2910m <abhi2910m>
2021-08-10 09:13:38 +0000

    Test commit

--- temperatures.py
+++ temperatures.py
@@ -52,3 +52,16 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is a new markdown comment
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+
