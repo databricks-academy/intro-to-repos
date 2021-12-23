dddfe7e7a879b02784bd9a2e5f195e4c523357ec
dpandey-db <dpandey-db>
2021-12-23 20:17:27 +0000

    Testing a commit and push from repos

--- temperatures.py
+++ temperatures.py
@@ -52,3 +52,8 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is a new markdown cell to test edits
