cc02914b6467e36734843f918787a9f3f46559e7
Sarvagna Srikari <sarvagna.m@gmail.com>
2022-01-06 16:18:07 +0530

    Update my_funcs.py

--- my_lib/my_funcs.py
+++ my_lib/my_funcs.py
@@ -1,3 +1,5 @@
+# Databricks Source Code
+
 def convertFtoC(unitCol, tempCol):
     from pyspark.sql.functions import when, col
     return when(col(unitCol) == "F", (col(tempCol) - 32) * (5/9)).otherwise(col(tempCol)).alias("temp_celcius")
