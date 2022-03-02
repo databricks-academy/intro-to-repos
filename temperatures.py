84f7b11a2752e4667454dfc269ac3ac0da8ea709
VarunRauthan <varun.rauthan@gmail.com>
2022-03-02 17:50:52 +0530

    Update temperatures.py

--- temperatures.py
+++ temperatures.py
@@ -52,12 +52,3 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC trying out db repo push
-
-# COMMAND ----------
-
-df.count()
