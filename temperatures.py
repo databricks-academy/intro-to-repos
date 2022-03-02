3669ba1f387f48419173126bc1a4349fd7126cd5
varun.rauthan@gmail.com <varun.rauthan@gmail.com>
2022-03-02 11:10:13 +0000

    db repo test commit

--- temperatures.py
+++ temperatures.py
@@ -52,3 +52,12 @@ display(df.select(roundedTemp("unit", "temp")))
 # COMMAND ----------
 
 display(df.select(convertFtoC("unit", "temp")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is markdown
+
+# COMMAND ----------
+
+df.count()
